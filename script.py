3391da307e476d8428e77d7bfd7ab515321fec0d
Yurii Mashtalir <mashtaliryurko@gmail.com>
2017-02-01 13:56:35 +0200

    Yards commit

--- script.py
+++ script.py
@@ -5,20 +5,23 @@ from area import area
 
 # for roads
 
-file_name = 'roadsGeo.json'
+file_name = 'yardsGeo.json'
 
 with open(file_name) as infile:
     json_file = json.load(infile)
 
 my_feature_collection = geojson.FeatureCollection([])
 
+categories = ["open", "hard to reach", "unreachable"]
+
 for row in json_file:
-    # print row
+    print row
+    category = categories[row[0]]
     coos = []
-    for point in row:
+    for point in row[1]:
         coos.append((point['lng'], point['lat']))
     current_geometry = geojson.Polygon([coos])
-    # print current_geometry
+    print current_geometry
     current_area = area(current_geometry)
     my_feature_collection["features"].append(geojson.Feature(geometry=current_geometry, properties={
         "area": current_area}))
