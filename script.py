cbca9e91f389ad26f4a8cd8aee2accb997d7fe65
Yurii Mashtalir <mashtaliryurko@gmail.com>
2017-02-01 13:48:57 +0200

    Houses commit

--- script.py
+++ script.py
@@ -3,28 +3,25 @@ import geojson
 import math
 from area import area
 
-# for first floor
+# for houses
 
-file_name = 'firstFloorFunctionGeo.json'
+file_name = 'housesGeo.json'
 
 with open(file_name) as infile:
     json_file = json.load(infile)
 
 my_feature_collection = geojson.FeatureCollection([])
 
-floor_functions = ["office", "cafe", "garage", "culture", "housing","ruin"]
-
 for row in json_file:
-	print row[1]
-	floor_function = floor_functions[row[0]]
-	coos = []
-	for point in row[1]:
-		coos.append((point['lng'], point['lat']))
-	current_geometry = geojson.Polygon([coos])
-	# print current_geometry
-	current_area = area(current_geometry)
-	my_feature_collection["features"].append(geojson.Feature(geometry=current_geometry, properties={
-                                             "function": floor_function ,"area": current_area}))
+    # print row
+    coos = []
+    for point in row:
+        coos.append((point['lng'], point['lat']))
+    current_geometry = geojson.Polygon([coos])
+    # print current_geometry
+    current_area = area(current_geometry)
+    my_feature_collection["features"].append(geojson.Feature(geometry=current_geometry, properties={
+        "area": current_area}))
 
 
 print my_feature_collection
